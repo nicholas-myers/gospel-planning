adf57c9b536e71df997c697b2a16309cc52248ef
Nick Myers <nicholas.myers.professional@gmail.comgit config --global user.name Nick>
2020-10-04 10:06:52 -0700

    set up basic component file structure and Home page with some basic content

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+        <header>
+          <h1>Myers Family History</h1>
+          <nav>
+            <a>Login</a>
+            <a>Logout</a>
+          </nav>
+        </header>
+        <section>Write in a journal. Upload pictures. Input Family Names. View Generation.</section>
+    </div>
+  );
+}
+
+export default Home;
