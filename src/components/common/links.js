a05f68f64e9f8afe5cf538f4a92c99c81ebc1fff
Nick Myers <nicholas.myers.professional@gmail.comgit config --global user.name Nick>
2020-10-04 15:48:20 -0700

    extracted links into links common file for cleaner code

---
+++ src/components/common/links.js
@@ -0,0 +1,18 @@
+export const homepaths = [
+    "/",
+    "/journal",
+    "/familychart",
+    "/familyplanning",
+    "/home-evening",
+  ];
+  export const homelinks = ["Home", "Journal", "Chart", "Plan", "Home Evening"];
+  export const journallinks = [
+    "Testimony",
+    "Impressions",
+    "Journal",
+    "General Conference",
+    "Sabbath",
+    "Talks",
+  ];
+
+export const journalpaths = ["/journal/testimony", "/journal/impressions", "/journal/journal", "/journal/generalconference", "/journal/sabbath", "/journal/talks"]
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,6 +6,7 @@ import LandingPage from "./LandingPage";
 import FamilyChart from "../FamilyChart/FamilyChart";
 import FamilyPlanning from "../FamilyPlanning/FamilyPlanning";
 import HomeEvening from "../HomeEvening/HomeEvening";
+import { homepaths, homelinks, journallinks, journalpaths} from "../../common/links"
 
 const { Header, Content, Sider } = Layout;
 
@@ -13,15 +14,6 @@ function Home() {
   const history = useHistory();
   const location = useLocation();
   console.log(location.pathname);
-
-  const paths = [
-    "/",
-    "/journal",
-    "/familychart",
-    "/familyplanning",
-    "/home-evening",
-  ];
-  const homelinks = ["Home", "Journal", "Chart", "Plan", "Home Evening"];
 
   const handleLink = (path) => {
     history.push(path);
@@ -50,7 +42,7 @@ function Home() {
         <nav>
           {homelinks.map((link, index) => {
             return (
-              <Button onClick={() => handleLink(paths[index])}>{link}</Button>
+              <Button onClick={() => handleLink(homepaths[index])}>{link}</Button>
             );
           })}
           {profile == false ? (
@@ -87,7 +79,20 @@ function Home() {
           <Journal />
         </Route>
         <Sider style={{ backgroundColor: "#534239", color: "#715a4e" }}>
-          Side Nav goes here.
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              alingItems: "center",
+              width: "100%",
+            }}
+          >
+            {location.pathname == "/journal" &&
+              journallinks.map((link) => {
+                return <Button style={{ margin: "1rem" }}>{link}</Button>;
+              })}
+          </div>
         </Sider>
       </Content>
     </Layout>
